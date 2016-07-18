31fb5362c0003ccb96964d751c18ad1b50bde0ac
Dev <dev.rangarajan@gmail.com>
2016-07-17 18:53:02 -0700

    Initial resources + layout

---
+++ AdvancedScouting.js
@@ -0,0 +1,15 @@
+/*Scouting Metrics and Functions*/
+/*
+TODO: Team Object
+TODO: Scores
+TODO: Big Data Storage
+TODO: Prediction, ML Classification Problem
+TODO: Load in thetas for prediction from web AI
+TODO: Game Independence
+TODO: FTC/FRC switch
+TODO: Offense Metric
+TODO: Defense Metric
+TODO: Auto Metric
+TODO: Hook team to visualization
+TODO: Verbose Data, OTF processing
+*/
